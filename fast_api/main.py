ed9c4ce013bae846b15708f0843b9e5e579f6fd3
Maulana Alfaruqi <alfaruqimaulana1@gmail.com>
2026-07-08 12:05:28 +0300

    connect to openweather api

--- fast_api/main.py
+++ fast_api/main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+# weather.py is the worker that fetches the data, main.py is the receptionist of the FastAPI application
 
-app = FastAPI()
+from fastapi import FastAPI, HTTPException
+from weather import get_local_weather, LocalWeather
 
+# 1. Create the main FastAPI application instance
+app = FastAPI(title="Smart-Inventory-System")
+
+# 2. A simple home route just to test if the server is alive
 @app.get("/")
-def read_root():
-    return {"message": "Hello from FastAPI"}  
+def home():
+    return {"status": "Successfully running!"}
+
+# 3. The actual API route that Django will talk to
+@app.get("/api/weather/future", response_model=LocalWeather)
+
+def fetch_tomorrow_weather():
+    # Call the worker function from weather.py
+    weather_info = get_local_weather()
+
+    if not weather_info:
+        raise HTTPException(status_code=500, detail="Failed to fetch forecast from OpenWeatherMap")
+        
+    return weather_info # FastAPI turns this into clean JSON automatically
---
+++ fast_api/weather.py
@@ -0,0 +1,40 @@
+import os
+import requests
+from dotenv import load_dotenv
+from pydantic import BaseModel
+
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+
+# Read coordinates dynamically from the env configuration
+LAT = os.getenv('STORE_LAT')
+LON = os.getenv('STORE_LON')
+
+class LocalWeather(BaseModel):
+    temperature: float
+    humidity: int
+    condition: str
+
+def get_local_weather():
+    url = f'https://api.openweathermap.org/data/2.5/forecast?lat={LAT}&lon={LON}&appid={API_KEY}&units=metric' # use "forecast" to call the 5-day prediction instead of current weather
+    
+    try:
+        response = requests.get(url).json()
+        forecast_list = response.get('list', [])  # This is the big box of 40 packets (Each packet is 3 hours apart)
+        
+        # grab the specific time packet (number 8 --24 hours into the future)
+        tomorrow_data = forecast_list[8] 
+        
+        # and then we dig inside that packet to grab the specific data
+        main_data = tomorrow_data.get('main', {})
+        weather_data = tomorrow_data.get('weather', [{}])[0] #grab first item
+        
+        # finally extract the values and return
+        return LocalWeather(
+            temperature = float(main_data.get('temp', 0.0)),
+            humidity = int(main_data.get('humidity', 0)),
+            condition = weather_data.get('main', 'Clear')
+        )
+    except Exception:
+        return None
+
