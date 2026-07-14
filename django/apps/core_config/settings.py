fcc02690651064a1ba51f44d41aa49ae1e909ab0
Maulana Alfaruqi <alfaruqimaulana1@gmail.com>
2026-07-14 16:59:39 +0300

    integrate PostgreSQL configuration in settings

--- django/apps/core_config/settings.py
+++ django/apps/core_config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'core_config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'inventory_db'),
+        'USER': os.getenv('DB_USER', 'alfa'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'alfaruqi2005.'),
+        'HOST': os.getenv('DB_HOST', '127.0.0.1'), # Switch to 'db' in Docker
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
