f5101f6bcb77ebf40cabe4365495001bec35cf3c
Maulana Alfaruqi <alfaruqimaulana1@gmail.com>
2026-07-16 18:21:45 +0300

    refactor: remove weather record from Serializer

--- django/apps/inventory/serializers.py
+++ django/apps/inventory/serializers.py
@@ -1,31 +1,25 @@
 
 from rest_framework import serializers
-from .models import Product, Sales, WeatherRecord, StockLevel, OrderPrediction
+from .models import Product, Sales, Shelf, OrderPrediction
 
 
 class ProductSerializer(serializers.ModelSerializer): 
     class Meta: 
         model = Product
-        fields = ["id", "name", "category", "expire_date", "shelf_life", "unit_cost", "selling_price"]
+        fields = ["id", "name", "shelf", "expire_date", "shelf_life", "quantity", "unit_cost", "selling_price", "is_deleted"]
         read_only_fields = ["id", "shelf_life"]
 
 class SalesSerializer(serializers.ModelSerializer): 
     class Meta: 
         model = Sales
-        fields = ["id", "date", "product", "quantity_sold", "total_revenue"]
+        fields = ["id", "created_at", "product", "quantity_sold", "total_revenue"]
         read_only_fields = ["id", "total_revenue"]
 
-class WeatherRecordSerializer(serializers.ModelSerializer): 
+class ShelfSerializer(serializers.ModelSerializer): 
     class Meta: 
-        model = WeatherRecord
-        fields = ["id", "date", "avg_temp", "humidity", "condition"]
-        read_only_fields = ["id"] # Dont make readonly! bcz weather API create the records, it needs to send a POST request to Django to save them
-
-class StockLevelSerializer(serializers.ModelSerializer): 
-    class Meta: 
-        model = StockLevel
-        fields = ["id", "product", "current_stock", "max_shelf_capacity"]
-        read_only_fields = ["id"]
+        model = Shelf
+        fields = ["id", "category", "current_stock", "max_shelf_capacity"]
+        read_only_fields = ["id", "current_stock"]
 
 class OrderPredictionSerializer(serializers.ModelSerializer): 
     class Meta: 
