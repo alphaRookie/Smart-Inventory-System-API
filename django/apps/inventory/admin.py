22681462d8dc2d98f99c9a7ef757b2db45f1d84d
Maulana Alfaruqi <alfaruqimaulana1@gmail.com>
2026-07-17 11:39:43 +0300

    feat: add django admin

--- django/apps/inventory/admin.py
+++ django/apps/inventory/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Product, Sales, Shelf, OrderPrediction
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "shelf", "expire_date", "shelf_life", "quantity", "unit_cost", "selling_price", "is_deleted") # use tuple(unchangeable) instead of list 
+    readonly_fields = ("shelf_life",)
+    list_filter = ("shelf",)
+    date_hierarchy = "expire_date" 
+    
+
+@admin.register(Sales)
+class SalesAdmin(admin.ModelAdmin):
+    list_display = ("id", "created_at", "product", "quantity_sold", "total_revenue")
+    list_filter = ("product",) # Comma needed, bcoz it's Tuple (a list of items) not string
+    date_hierarchy = "created_at"
+   
+
+@admin.register(Shelf)
+class ShelfAdmin(admin.ModelAdmin):
+    list_display = ("id", "category", "current_stock", "max_shelf_capacity") 
+    list_filter = ("category",)
+
+
+@admin.register(OrderPrediction)
+class OrderPredictionAdmin(admin.ModelAdmin):
+    list_display = ("id", "product", "demand_prediction", "order_suggestion", "target_timing")
+    list_filter = ("product",)
+    date_hierarchy = "target_timing"
