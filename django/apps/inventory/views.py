cdf3ba7d29c8275e18726266b9ff42218c693c1b
Maulana Alfaruqi <alfaruqimaulana1@gmail.com>
2026-07-18 12:17:17 +0300

    feat: Create Product views

--- django/apps/inventory/views.py
+++ django/apps/inventory/views.py
@@ -1,3 +1,72 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from typing import cast
 
-# Create your views here.
+from .models import Product, Shelf, Sales, OrderPrediction
+from .serializers import ProductSerializer, ShelfSerializer, SalesSerializer, OrderPredictionSerializer
+from .services import ProductService, ShelfService, SalesService, OrderPredictionService
+
+
+class ProductAPIView(APIView):
+
+    def get(self, request):
+        product = Product.objects.all() 
+        serializer = ProductSerializer(product, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = ProductSerializer(data = request.data) # JSON to Model
+        serializer.is_valid(raise_exception=True)
+        validated_data = cast(dict, serializer.validated_data) # To shut pylance complaint
+        product = ProductService.save_product(
+            product=None,
+            name=validated_data.get("name"),
+            shelf=validated_data.get("shelf"),
+            type=validated_data.get("type"),
+            expire_date=validated_data.get("expire_date"),
+            quantity=validated_data.get("quantity"),
+            unit_cost=validated_data.get("unit_cost"),
+            selling_price=validated_data.get("selling_price"),
+        )
+        return Response({
+            "message": "New product added",
+            "product": ProductSerializer(product).data 
+        }, status=status.HTTP_201_CREATED)
+    
+    
+class ProductItemAPIView(APIView):
+
+    def get(self, request, pk):
+        product = get_object_or_404(Product, pk=pk) 
+        serializer = ProductSerializer(product) # no need `many=True` bcoz return single obj (Model to JSON)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def patch(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        serializer = ProductSerializer(product, data = request.data, partial=True) # enable PATCH (update some instead all)
+        serializer.is_valid(raise_exception=True)
+        validated_data = cast(dict, serializer.validated_data)
+        updated_product = ProductService.save_product(
+            product=product,
+            name=validated_data.get("name"),
+            shelf=validated_data.get("shelf"),
+            type=validated_data.get("type"),
+            expire_date=validated_data.get("expire_date"),
+            quantity=validated_data.get("quantity"),
+            unit_cost=validated_data.get("unit_cost"),
+            selling_price=validated_data.get("selling_price"),
+        )
+        return Response({
+            "message": "Product updated",
+            "product": ProductSerializer(updated_product).data
+        }, status=status.HTTP_200_OK) 
+
+    def delete(self, request, pk):
+        product = get_object_or_404(Product, pk=pk) 
+        shelf = get_object_or_404(Shelf, product=product) 
+        ProductService.delete_product(product=product, shelf=shelf)
+        return Response({"message": f"{product.name} deleted"},status=status.HTTP_200_OK)
+
