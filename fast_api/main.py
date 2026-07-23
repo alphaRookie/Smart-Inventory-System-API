f9649cb4b58bc4ea9c126567fe475a83a0077c02
Maulana Alfaruqi <alfaruqimaulana1@gmail.com>
2026-07-23 09:49:15 +0300

    feat: implement async weather fetching using httpx and await

--- fast_api/main.py
+++ fast_api/main.py
@@ -3,22 +3,23 @@
 from fastapi import FastAPI, HTTPException
 from weather import get_local_weather, LocalWeather
 
-# 1. Create the main FastAPI application instance
+# Create the main FastAPI application instance
 app = FastAPI(title="Smart-Inventory-System")
 
-# 2. A simple home route just to test if the server is alive
+# A simple home route just to test if the server is alive
 @app.get("/")
-def home():
+async def home():
     return {"status": "Successfully running!"}
 
-# 3. The actual API route that Django will talk to
-@app.get("/api/weather/future", response_model=LocalWeather)
 
-def fetch_tomorrow_weather():
-    # Call the worker function from weather.py
-    weather_info = get_local_weather()
+# The API route to fetch the weather data
+@app.get("/api/fetch-weather", response_model=LocalWeather)
+async def fetch_tomorrow_weather():
+    # Call the worker function from weather.py 
+    weather_info = await get_local_weather() # use 'await' for async
 
     if not weather_info:
         raise HTTPException(status_code=500, detail="Failed to fetch forecast from OpenWeatherMap")
-        
     return weather_info # FastAPI turns this into clean JSON automatically
+
+ 
