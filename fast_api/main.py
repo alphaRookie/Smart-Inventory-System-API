a62b5503b13e490db2e4cbc068f241407636aef8
Maulana Alfaruqi <alfaruqimaulana1@gmail.com>
2026-07-24 14:45:25 +0300

    feat: create endpoint to run the whole order prediction process
    Created prediction schemas, demand calculation logic, and the POST /api/predict endpoint to help Django calculate stock orders using weather forecasts

--- fast_api/main.py
+++ fast_api/main.py
@@ -2,6 +2,8 @@
 
 from fastapi import FastAPI, HTTPException
 from weather import get_local_weather, LocalWeather
+from pydantic import BaseModel
+from ml_engine import predict_demand
 
 # Create the main FastAPI application instance
 app = FastAPI(title="Smart-Inventory-System")
@@ -21,5 +23,45 @@ async def fetch_tomorrow_weather():
     if not weather_info:
         raise HTTPException(status_code=500, detail="Failed to fetch forecast from OpenWeatherMap")
     return weather_info # FastAPI turns this into clean JSON automatically
+
+
+# This is the request data template that Django must send to FastAPI
+class PredictionRequest(BaseModel):
+    product_name: str
+    product_type: str
+    base_demand: int
+    current_stock: int
+
+# This is what FastAPI will give back to Django
+class PredictionResponse(BaseModel):
+    product_name: str
+    predicted_demand: int
+    suggested_order: int
+
+# Endpoint for order suggestion
+@app.post("/api/predict", response_model=PredictionResponse)
+async def get_inventory_prediction(request_data: PredictionRequest):
+    # 1. Fetch tomorrow's weather automatically
+    weather_info = await get_local_weather() 
+    if not weather_info:
+        raise HTTPException(status_code=500, detail="Failed to fetch forecast from OpenWeatherMap")
+        
+    # 2. Run AI calculation logic using tomorrow's temperature
+    prediction = predict_demand(
+        base_demand=request_data.base_demand,
+        product_type=request_data.product_type,
+        temperature=weather_info.temperature
+    )
+    
+    # 3. Calculate suggested order (Demand minus what we already have on shelves)
+    suggestion = prediction - request_data.current_stock
+    
+    # 4. Return everything back to Django cleanly
+    return PredictionResponse(
+        product_name=request_data.product_name,
+        predicted_demand=prediction,
+        suggested_order=max(0, suggestion)  # Don't suggest negative order counts
+    )
+
 
  
