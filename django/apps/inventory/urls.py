2cbfa70286863e2d8e13fbd41f9b4cf51b2240c7
Maulana Alfaruqi <alfaruqimaulana1@gmail.com>
2026-08-13 10:17:49 +0300

    refactor: change URL path slashes position

--- django/apps/inventory/urls.py
+++ django/apps/inventory/urls.py
@@ -2,23 +2,23 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("/product", views.ProductAPIView.as_view(), name="list-products"),
-    path("/product/<int:pk>", views.ProductItemAPIView.as_view(), name="detail-products"),
+    path("product", views.ProductAPIView.as_view(), name="list-products"),
+    path("product/<int:pk>", views.ProductItemAPIView.as_view(), name="detail-products"),
 
-    path("/sales", views.SalesAPIView.as_view(), name="list-sales"),
-    path("/sales/<int:pk>", views.SalesItemAPIView.as_view(), name="detail-sales"),
+    path("sales", views.SalesAPIView.as_view(), name="list-sales"),
+    path("sales/<int:pk>", views.SalesItemAPIView.as_view(), name="detail-sales"),
     
-    path("/shelf", views.ShelfAPIView.as_view(), name="list-shelf"),
-    path("/shelf/<int:pk>", views.ShelfItemAPIView.as_view(), name="detail-shelf"),
+    path("shelf", views.ShelfAPIView.as_view(), name="list-shelf"),
+    path("shelf/<int:pk>", views.ShelfItemAPIView.as_view(), name="detail-shelf"),
 
-    path("/predictions", views.OrderPredictionAPIView.as_view(), name="list-all-prediction"),
-    path("/predictions/<int:pk>", views.OrderPredictionItemAPIView.as_view(), name="detail-prediction"),
+    path("predictions", views.OrderPredictionAPIView.as_view(), name="list-all-prediction"),
+    path("predictions/<int:pk>", views.OrderPredictionItemAPIView.as_view(), name="detail-prediction"),
 
     # endpoint to run the whole prediction process
-    path("/predict/<int:product_id>", views.SingleOrderPredictionView.as_view(), name="single-prediction-process"), 
-    path("/predict-batch", views.BatchOrderPredictionView.as_view(), name="batch-prediction-process"), 
+    path("predict/<int:product_id>", views.SingleOrderPredictionView.as_view(), name="single-prediction-process"), 
+    path("predict-batch", views.BatchOrderPredictionView.as_view(), name="batch-prediction-process"), 
 
-    path("/spoilage-check", views.SpoilageCheckView.as_view(), name="spoilage-check"),
-    path("/spoilage-notif", views.SpoilageNotificationAPIView.as_view(), name="spoilage-check"),
-    path("/spoilage-notif/<int:pk>", views.SpoilageNotificationItemAPIView.as_view(), name="spoilage-check-detail")
+    path("spoilage-check", views.SpoilageCheckView.as_view(), name="spoilage-check"),
+    path("spoilage-notif", views.SpoilageNotificationAPIView.as_view(), name="spoilage-check"),
+    path("spoilage-notif/<int:pk>", views.SpoilageNotificationItemAPIView.as_view(), name="spoilage-check-detail")
 ]
