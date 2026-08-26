74ca69a3993d195ef37e1f5afce675184b7d7f99
Maulana Alfaruqi <alfaruqimaulana1@gmail.com>
2026-08-26 16:33:54 +0300

    feat: update ProductSerializer to support M2M shelf allocations

--- django/apps/inventory/serializers.py
+++ django/apps/inventory/serializers.py
@@ -1,12 +1,24 @@
 
 from rest_framework import serializers
-from .models import Product, Sales, Shelf, OrderPrediction, SpoilageNotification
+from .models import Product, Sales, Shelf, OrderPrediction, SpoilageNotification, ProductShelf
 
+class ProductShelfSerializer(serializers.ModelSerializer):
+    # "number passed here is referring to shelf.id as actual Shelf object (not just plain number)"
+    shelf = serializers.PrimaryKeyRelatedField(queryset=Shelf.objects.all()) 
+    class Meta:
+        model = ProductShelf
+        fields = ["shelf", "quantity"] # no need "id" for post/pacth payload
 
 class ProductSerializer(serializers.ModelSerializer): 
+    # Why did i create shelf_allocations?
+    # To allow the user to split 1 product across multiple shelves (e.g., 50 units on Shelf 1, 30 units on Shelf 2)
+    shelf_allocations = ProductShelfSerializer(
+        many=True, 
+        required=False
+    )
     class Meta: 
         model = Product
-        fields = ["id", "name", "shelf", "expire_date", "shelf_life", "quantity", "unit_cost", "type", "selling_price", "is_deleted", "is_expired"]
+        fields = ["id", "name", "type", "expire_date", "shelf_life", "quantity", "shelf_allocations", "unit_cost", "selling_price", "is_deleted", "is_expired"] # replace 'shelves' field with 'shelf_allocations'
         read_only_fields = ["id", "shelf_life", "is_expired"]
 
 class SalesSerializer(serializers.ModelSerializer): 
