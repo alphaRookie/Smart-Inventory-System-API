06b27cb6eebe1f534af44a4494f237cd08f0a09c
Maulana Alfaruqi <alfaruqimaulana1@gmail.com>
2026-08-27 14:57:41 +0300

    feat: update Product admin to support M2M and register ProductShelf too

--- django/apps/inventory/admin.py
+++ django/apps/inventory/admin.py
@@ -1,13 +1,30 @@
 from django.contrib import admin
-from .models import Product, Sales, Shelf, OrderPrediction
+from .models import Product, Sales, Shelf, OrderPrediction, ProductShelf
+
+
+class ProductShelfInline(admin.TabularInline):
+    """Allows managing shelf allocations directly inside the Product admin page"""
+    model = ProductShelf
+    extra = 1
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "shelf", "expire_date", "shelf_life", "quantity", "unit_cost", "type", "selling_price", "is_deleted") # use tuple(unchangeable) instead of list 
+    list_display = ("id", "name", "get_shelves", "expire_date", "shelf_life", "quantity", "unit_cost", "type", "selling_price", "is_expired", "is_deleted") # use tuple(unchangeable) instead of list 
     readonly_fields = ("shelf_life",)
-    list_filter = ("shelf",)
+    list_filter = ("shelves",) # filter by M2M field name
     search_fields = ("name",) 
     date_hierarchy = "expire_date" 
+    inlines = [ProductShelfInline]
+
+    @admin.display(description="Shelves")
+
+    # Custom method to display assigned shelves in a list separated by comma
+    def get_shelves(self, obj):
+        return ", ".join([str(shelf) for shelf in obj.shelves.all()])
+
+    # Optimize query performance to prevent N+1 queries when loading list_display
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related("shelves")
     
 
 @admin.register(Sales)
@@ -28,3 +45,9 @@ class OrderPredictionAdmin(admin.ModelAdmin):
     list_display = ("id", "product", "demand_prediction", "order_suggestion", "target_timing")
     list_filter = ("product",)
     date_hierarchy = "target_timing"
+
+
+@admin.register(ProductShelf)
+class ProductShelfAdmin(admin.ModelAdmin):
+    list_display = ("id", "product", "shelf", "quantity")
+    list_filter = ("product", "shelf")
