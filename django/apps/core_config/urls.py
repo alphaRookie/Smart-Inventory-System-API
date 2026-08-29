ae92a1e5ede800e0d69f8c12d7214679458dd9ce
Maulana Alfaruqi <alfaruqimaulana1@gmail.com>
2026-08-29 10:34:33 +0300

    feat: create URL path for Swagger&Redoc

--- django/apps/core_config/urls.py
+++ django/apps/core_config/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("inventory/", include("inventory.urls")),
+
+    #Swagger/Redoc
+    path('api/schema', SpectacularAPIView.as_view(), name='schema'), # This generates the "Schema" (the raw data)
+    path('api/docs/swagger', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'), # This is the INTERACTIVE one (Swagger)
+    path('api/docs/redoc', SpectacularRedocView.as_view(url_name='schema'), name='redoc'), # This is the BEAUTIFUL one (Redoc)
 ]
