4c3ac0b97bb7bd83831156c251e5234dc404074e
Maulana Alfaruqi <alfaruqimaulana1@gmail.com>
2026-09-17 11:13:31 +0300

    feat: create Routes and Navbar for Product

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,27 @@
-/* The main root component of your user interface. It holds the layout structure and renders your child components (pages, navbars, forms) */
+/* The main root component of user interface. It holds the layout structure and renders components (pages, navbars, forms) */
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
-import './App.css'
+// Import pages
+import DashboardPage from './pages/DashboardPage'; 
+import ProductsPage from './pages/products/ProductsPage';
+import ProductDetailPage from './pages/products/ProductDetailPage';
 
-function App() {
+
+export default function App() {
   return (
-    <div>
-      <h1>Smart Inventory System</h1>
-      <p>Frontend environment ready.</p>
-    </div>
-  )
+    <BrowserRouter>
+      {/* Navigation Bar (Changes the URL path when clicked without refresh the page) */}
+      <nav style={{ display: 'flex', gap: '15px', padding: '10px', background: '#eee' }}>
+        <Link to="/">Dashboard</Link>
+        <Link to="/products">Products</Link>
+      </nav>
+
+      {/* URL Routes (listens to that URL change and decides which React component to render) */}
+      <Routes>
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products/:id" element={<ProductDetailPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
-
